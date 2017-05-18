9e49b322220d144df88175af7bd83dba80bd390c
Hc747 <harrisoncole05@gmail.com>
2017-05-18 17:22:07 +1000

    Implementation of SOAP login method

--- src/uts/wsd/soap/FlightCenterAPI.java
+++ src/uts/wsd/soap/FlightCenterAPI.java
@@ -5,12 +5,15 @@ import uts.wsd.util.ResponseWrapper;
 
 import javax.annotation.Resource;
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.xml.ws.WebServiceContext;
 import javax.xml.ws.WebServiceException;
 import javax.xml.ws.handler.MessageContext;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Harrison, Alias: Hc747, Contact: harrisoncole05@gmail.com
@@ -34,6 +37,38 @@ public class FlightCenterAPI {
 		session.invalidate();
 
 		return context.getUser().isAuthenticated() ? ResponseWrapper.success() : ResponseWrapper.failure(ERR_LOGGED_OUT);
+	}
+
+	@WebMethod(operationName = "login")
+	public ResponseWrapper login(@WebParam(name = "email") String email,
+								 @WebParam(name = "password") String password) {
+
+		SessionContext context = SessionContext.contextualise(session());
+
+		if (context.getUser().isAuthenticated())
+			return ResponseWrapper.failure(ERR_LOGGED_IN);
+
+		final List<String> messages = new ArrayList<>();
+
+		//TODO
+		//properly sanitise and validate all parameters
+		if (email == null || email.isEmpty())//TODO inadequate
+			messages.add("You must provide a valid email address.");
+
+		if (password == null || password.isEmpty())//TODO inadequate
+			messages.add("You must provide a valid password.");
+
+		if (!messages.isEmpty())
+			return ResponseWrapper.failure(messages);
+
+		//TODO
+		//ensure a user with matching credentials exists
+
+		//TODO
+		//if the above does not fail i.e, user != null
+		//update the context's user object
+
+		return ResponseWrapper.success();//TODO welcome, username
 	}
 
 	private HttpSession session() {
